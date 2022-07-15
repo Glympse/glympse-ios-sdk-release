414ad45fbe3691a6a5815797bf493c4ceda2ccbe
Nick Glickenhouse <nick@glympse.com>
2022-07-15 10:43:52 -0400

    2.105.228

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GlympseAPI",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "GlympseKit",
+            targets: ["GlympseKit", "GlympsePrivateAPI"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "GlympseKit",
+            url: "https://github.com/Glympse/glympse-ios-sdk-release/releases/download/2.105.228/GlympseKit.zip",
+            checksum: "22b8684d309dfbef7a47907595bbcadce84a6b5775e479125cfef397c61039b2"
+        ),
+        .binaryTarget(
+            name: "GlympsePrivateAPI",
+            url: "https://github.com/Glympse/glympse-ios-sdk-release/releases/download/2.105.228/GlympsePrivateAPI.zip",
+            checksum: "ea07155448d2192eb1dcbffa5eb4d11889bbeaafc5466d03ca611436a2199265"
+        ),
+    ]
+)
