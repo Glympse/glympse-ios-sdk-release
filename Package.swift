572ca597fae8ad4575396f156e38d29032afe86e
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2023-08-31 18:56:47 +0000

    2.107.253

--- Package.swift
+++ Package.swift
@@ -23,13 +23,13 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "GlympseKit",
-            url: "https://github.com/Glympse/glympse-ios-sdk-release/releases/download/2.107.252/GlympseKit.zip",
-            checksum: "bc5d35c7ae55231f3aca82db4c4aceb50dd3996793719a19e24433d3ad6f6116"
+            url: "https://github.com/Glympse/glympse-ios-sdk-release/releases/download/2.107.253/GlympseKit.zip",
+            checksum: "9f56c32b8a4b56c0f1632dad07200dc033942153dcd8f7dcce871869dc6d6391"
         ),
         .binaryTarget(
             name: "GlympsePrivateAPI",
-            url: "https://github.com/Glympse/glympse-ios-sdk-release/releases/download/2.107.252/GlympsePrivateAPI.zip",
-            checksum: "5bc8af760e1e14a804c004af7b821ae5db2246dfb8654ebb54f5ad812a1a5080"
+            url: "https://github.com/Glympse/glympse-ios-sdk-release/releases/download/2.107.253/GlympsePrivateAPI.zip",
+            checksum: "dc76e7fdff1a9c5b5a4c2879ff1f080e79d02d5b7a3433c6716389f8898ef747"
         ),
     ]
 )
